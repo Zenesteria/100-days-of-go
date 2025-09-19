b2228798b159eced79ad49c2b86a35a8e4de174f
Zenesteria <abdurrahman.aderinto@gmail.com>
2025-09-19 16:39:10 +0100

    feat: functions

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "learning-path/functions"
+import "fmt"
 
 func main() {
-	functions.Functions()
+	var i1 int
+	var f1 float32
+
+	i1, _, f1 = ThreeValues()
+
+	fmt.Println(i1, f1)
+
+}
+
+func ThreeValues() (int, int, float32) {
+	return 5, 2, 4.5
 }
