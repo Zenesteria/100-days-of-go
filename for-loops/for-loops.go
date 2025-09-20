59c7eb7f651d9ecc365f9e4e989a50b5ea7aa3c1
Zenesteria <abdurrahman.aderinto@gmail.com>
2025-09-20 06:11:47 +0100

    feat: go for loops

--- for-loops/for-loops.go
+++ for-loops/for-loops.go
@@ -1,10 +1,40 @@
 package forloops
 
-func ForLoops() {
-	for i := 1; i<=60; i++{
-		if i%3 ==0 && i%5 ==0{
-			continue 
+func contains(arr []int, target int) bool {
+	result := false
+	for _, val := range arr {
+		if val == target {
+			result = true
+		}
+	}
+	return result
+}
+
+func TotalFruit(fruits []int) int {
+
+	maxLength := 0
+
+	for i := range fruits {
+		var basketOne = []int{}
+		var basketTwo = []int{}
+		for j := i; j < len(fruits); j++ {
+			if contains(basketOne, fruits[j]) || len(basketOne) == 0 {
+				basketOne = append(basketOne, fruits[j])
+				continue
+			}
+			if contains(basketTwo, fruits[j]) || len(basketTwo) == 0 {
+				basketTwo = append(basketTwo, fruits[j])
+				continue
+			}
+
+			break
+
+		}
+		if len(basketTwo)+len(basketOne) > maxLength {
+			maxLength = len(basketTwo) + len(basketOne)
 		}
 	}
 
+	// Replace this placeholder return statement with your code
+	return maxLength
 }
